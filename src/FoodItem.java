f026520f2b97ff0fc621fb78190a4f74f2edff1a
Eamonn <21204616@studentmail.ul.ie>
2022-10-19 17:45:07 +0100

    Added some data types to FoodItem

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -1,2 +1,16 @@
 public class FoodItem extends FoodCategory{
+    private String dishName;
+    private double dishCost;
+    private boolean isVegan;
+    private boolean containsMeat;
+    private boolean containsGluten;
+    private boolean containsCrustaceans;
+    private boolean containsEggs;
+    private boolean containsFish;
+    private boolean containsPeanuts;
+    private boolean containsSoyBeans;
+    private boolean containsDairy;
+    private boolean containsNuts;
+
+
 }
