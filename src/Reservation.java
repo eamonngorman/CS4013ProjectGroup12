9fe7cdf44d2d3a12422c9518a2a80c98b0c18074
Eamonn <21204616@studentmail.ul.ie>
2022-10-22 20:33:29 +0100

    Created a constructor for the Reservation class

--- src/Reservation.java
+++ src/Reservation.java
@@ -2,10 +2,18 @@ import java.time.LocalDateTime;
 
 public class Reservation {
     private String reservationName;
-    private int reservationId;
+    private int reservationId; // There might be a better way of implementing this. Ideas? - Eamonn
     private LocalDateTime reservationDate;
     private String phoneNumber;
     private int numPeople;
+
+    public Reservation(String reservationName, LocalDateTime reservationDate, String phoneNumber, int numPeople){
+        this.reservationName = reservationName;
+        this.reservationDate = reservationDate;
+        this.phoneNumber = phoneNumber;
+        this.numPeople = numPeople;
+    }
+
 
     public void sendReminderToCustomer(){
 
