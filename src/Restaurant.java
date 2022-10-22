59b15d6de666bbba7c7d737b12c90ce20b9d24c1
Eamonn <21204616@studentmail.ul.ie>
2022-10-22 20:37:17 +0100

    Deleted the unnecessary Tables class and entered it's code into the Restaurant class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class Restaurant {
     private Menu menu;
-    private int[] restaurantTables;
+    // private int[] restaurantTables; This mightn't be needed anymore
+    private ArrayList<Table> tables;
     // Wrapper class conversion might be needed for interaction with Staff.staffId
     private ArrayList<Integer> restaurantStaff;
     private ArrayList<Bill> restaurantBills;
