5203bd15ada7710652f59f631ff1bcc2a596786a
Liam Murray <LiamMurray3195@gmail.com>
2022-10-24 15:43:56 +0100

    Added a basic contructor and toString method. also wotrh mentioning, is isAvailable and isReserved not the same thing? no need for both

--- src/Table.java
+++ src/Table.java
@@ -3,4 +3,26 @@ public class Table {
     private boolean isReserved;
     private boolean isAvailable;
     private int canSeatXCustomers;
+
+    Table(int tableNum, int canSeatXCustomers){
+        
+        this.canSeatXCustomers = canSeatXCustomers;
+        this.tableNo = tableNum;
+    }
+
+    public int getTableNo(){
+        return tableNo;
+    }
+
+    public int getNumSeats(){
+        return canSeatXCustomers;
+    }
+
+    public String toString(){
+        
+        String print = "Table: " + getTableNo() + "\n" +
+        "can seat: " + getNumSeats();
+
+        return print;
+    }
 }
