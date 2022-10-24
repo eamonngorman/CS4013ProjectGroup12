8f6dbddbf519e8264e7fe9bab0c6b0d00d00488d
Liam Murray <LiamMurray3195@gmail.com>
2022-10-24 15:40:01 +0100

    Added a 'QuickTest' class, this is just to be used for testing changes and should not include any permanent code, it will be overridden

---
+++ src/QuickTest.java
@@ -0,0 +1,16 @@
+public class QuickTest {
+
+    public static void main(String[] args) {
+
+        /*
+         * I'm using this class just to run quick tests on any changes,
+         * do NOT write any permanent code here. It will be deleted!!
+         */
+    Table t1 = new Table(1, 6);
+    Table t2 = new Table(2, 1);
+
+    System.out.println(t1.toString());
+    System.out.println(t2.toString());
+
+}
+}
