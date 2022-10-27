110eea45336d913c8a78a54980d740f2291f6d95
Eamonn <21204616@studentmail.ul.ie>
2022-10-27 17:34:26 +0100

    - Moved seatWalkInCustomers from Staff class to FOHStaff class

--- src/FOHStaff.java
+++ src/FOHStaff.java
@@ -1,2 +1,6 @@
 public class FOHStaff extends Staff{
+
+    public void seatWalkInCustomers(){
+
+    }
 }
--- src/Staff.java
+++ src/Staff.java
@@ -1,7 +1,4 @@
 public class Staff extends Person{
     private int staffId;
 
-    public void seatWalkInCustomers(){
-
-    }
 }
