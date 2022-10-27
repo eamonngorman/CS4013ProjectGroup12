348008d24425df98fadd008f5ce8e5212939ad72
Eamonn <21204616@studentmail.ul.ie>
2022-10-27 17:04:42 +0100

    - Added getter for tables in Restaurant class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 public class Restaurant {
     private Menu menu;
     // private int[] restaurantTables; This mightn't be needed anymore
-    private ArrayList<Table> tables;
+    private  ArrayList<Table> tables;
     // Wrapper class conversion might be needed for interaction with Staff.staffId
     private ArrayList<Integer> restaurantStaff;
     private ArrayList<Bill> restaurantBills;
     private ArrayList<Reservation> reservations;
+
+    public ArrayList<Table> getTables() {
+        return tables;
+    }
 }
