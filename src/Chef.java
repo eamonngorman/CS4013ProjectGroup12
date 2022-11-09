ddd95cb1ac6d65651b6e0e6cb6c7f7c689775fc8
Eamonn <21204616@studentmail.ul.ie>
2022-11-09 12:35:32 +0000

    - Created a function for the Chef to change the orderStatus of an Order. - Added comment on future work to be done on this class

--- src/Chef.java
+++ src/Chef.java
@@ -1,7 +1,10 @@
 public class Chef extends Staff{
 
+/* Need to set the potential statuses the chef can change to, so
+they can't change the status to "Served" as that should only
+ be done by the WaitingPerson */
 
-    public void changeOrderStatus(){
-
+    public void changeOrderStatus(Order order, int statusCode){
+        order.setOrderStatus(order.getStatuses()[statusCode]);
     }
 }
