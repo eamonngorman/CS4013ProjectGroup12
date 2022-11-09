622dfe435427587d0c57119178543918ae07b6b5
Eamonn <21204616@studentmail.ul.ie>
2022-11-09 13:57:59 +0000

    - Made the ArrayList of tables and it's getter static

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class Restaurant {
     private Menu menu;
     // private int[] restaurantTables; This mightn't be needed anymore
-    private  ArrayList<Table> tables;
+    private static ArrayList<Table> tables;
     // Wrapper class conversion might be needed for interaction with Staff.staffId
     private ArrayList<Integer> restaurantStaff;
     private ArrayList<Bill> restaurantBills;
     private static ArrayList<Reservation> reservations;
 
-    public ArrayList<Table> getTables() {
+    public static ArrayList<Table> getTables() {
         return tables;
     }
 
@@ -20,4 +20,5 @@ public class Restaurant {
     public static ArrayList<Reservation> getReservations() {
         return reservations;
     }
+
 }
