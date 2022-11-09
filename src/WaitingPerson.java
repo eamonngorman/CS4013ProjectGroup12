1f1972748788c65ceebd6cd5a355e78956294221
Eamonn <21204616@studentmail.ul.ie>
2022-11-09 18:36:06 +0000

    - Partially implemented the createOrder function using the Order constructor and adding the Order to the Restaurants list (or map?) of orders - Added some parameters (orderNumber and foodItem) for the other functions in the class.

--- src/WaitingPerson.java
+++ src/WaitingPerson.java
@@ -1,14 +1,17 @@
 public class WaitingPerson extends Staff{
 
-
-    public void createOrder(){
-
+    // made this return the order number for tracking purposes. Maybe printing it to the screen would be sufficient?
+    public int createOrder(int tableNum){
+        Order newOrder = new Order(tableNum);
+        Restaurant.getRestaurantOrders().add(newOrder);
+        return newOrder.getOrderNumber();
 
     }
-    public void addItemToOrder(){
+    public void addItemToOrder(int orderNumber, FoodItem foodItem){
+        // Waiting to turn restaurantOrders into a map for ease of locating specific Orders to add FoodItems to
 
     }
-    public void deleteItemFromOrder(){
+    public void deleteItemFromOrder(int orderNumber, FoodItem foodItem){
 
     }
 }
