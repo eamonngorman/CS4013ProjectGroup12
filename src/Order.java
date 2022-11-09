d0d1a5340f338055c59730fa483cb94220db0514
Eamonn <21204616@studentmail.ul.ie>
2022-11-09 12:32:30 +0000

    - Created Order constructor which should create orders with incremented orderNumbers and will set the orderStatus to "Waiting for preparation" initially -Reworked the OrderStatus class to just be a String in the Order class that takes it's input from an array of mutually exclusive values in the statuses array - Added getters for orderNumber and the statuses array -Added setters for the orderNumber and orderStatus

--- src/Order.java
+++ src/Order.java
@@ -1,6 +1,33 @@
 import java.util.ArrayList;
 
 public class Order {
+    private static int count = 0;
+    private int orderNumber;
     private ArrayList<OrderedItem> order;
-    private OrderStatus orderStatus;
+    private String orderStatus;
+    private final String[] statuses = {"Waiting for preparation", "Being prepared", "Cooking", "Ready", "Served"};
+
+
+    public Order(){
+        this.setOrderNumber(++count);
+        this.orderStatus = statuses[0];
+
+    }
+
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String[] getStatuses() {
+        return statuses;
+    }
 }
