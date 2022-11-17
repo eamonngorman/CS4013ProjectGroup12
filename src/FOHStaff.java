05054db73c35e8154bf9892be687a65ffd1c830d
Conor <ccusack2002@gmail.com>
2022-11-17 17:04:01 +0000

    reservationReminder.java

--- src/FOHStaff.java
+++ src/FOHStaff.java
@@ -3,10 +3,10 @@ import java.util.ArrayList;
 public class FOHStaff extends Staff{
 
     public void seatWalkInCustomers(){
-
-    }
-    
-    public void seatWalkInCustomers(){
         showAvailableTables();
+    }
+
+    private void showAvailableTables() {
+
     }
 }
