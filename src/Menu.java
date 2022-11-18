39facae7343dfb6d2bac994ef0fb357ef8261280
Liam Murray <LiamMurray3195@gmail.com>
2022-11-18 07:51:47 +0000

    Fixed up Menu, FoodCategory & FoodItem

--- src/Menu.java
+++ src/Menu.java
@@ -2,16 +2,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Menu {
-    private ArrayList<FoodCategory> menuCategories;
-    private HashMap<String, Double> menuCost = new HashMap<String, Double>();
 
-    public void menuAdder(String category, String dishName, double dishCost){
+    private ArrayList<FoodCategory> menuCategories;
+    private String menuName;
+
+    Menu() {
+        this.menuName = "Menu";
+    }
+
+    Menu(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public void categoryAdder(FoodCategory category) {
+        menuCategories.add(category);
+    }
+
+    //what is this for?
+    public void menuAdder(String category, String dishName, double dishCost) {
         FoodItem addToMenu = new FoodItem(category, dishName, dishCost);
         menuCost.put(addToMenu.getDishName(), addToMenu.getDishCost());
     }
 
-    public void categoryAdder(FoodCategory category){
-        menuCategories.add(category);
-    }
 }
-
