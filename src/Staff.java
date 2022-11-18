e91cd292ddbc621bb4de5206c52253c0d556c7f4
Eamonn <21204616@studentmail.ul.ie>
2022-11-18 14:10:58 +0000

    - Added a constructor method for Staff that will be used to construct it's subclasses objects and a counter variable for the incrementation of staffId with new Staff objects.

--- src/Staff.java
+++ src/Staff.java
@@ -1,4 +1,11 @@
 public class Staff extends Person{
     private int staffId;
+    private int count = 1;
 
+    Staff(String name, String phoneNumber) {
+        super(name, phoneNumber);
+        this.staffId = count;
+        count++;
+        Restaurant.getStaff().add(this);
+    }
 }
