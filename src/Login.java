d9f71be5ac363bed04cf398455fcd75ed39d45a5
Conor <ccusack2002@gmail.com>
2022-11-19 16:50:27 +0000

    people login

---
+++ src/Login.java
@@ -0,0 +1,35 @@
+public class Login {
+
+    public String name;
+    public String userName;
+    public int password;
+    private boolean isStaff;
+
+    public Login(String Name, String UserName, int passwordHashed) {
+        name = Name;
+        userName = UserName;
+        password = passwordHashed;
+    }
+
+    public Login(String Name, String UserName, String passwordNotHashed) {
+        this(Name, UserName, passwordNotHashed.hashCode());
+    }
+
+    public Login(String Name, String UserName, int passwordHashed, boolean IsStaff) {
+        this(Name, UserName, passwordHashed);
+        if (IsStaff) isStaff = true;
+    }
+
+    public boolean getIsStaff() {
+        return isStaff;
+    }
+
+    public boolean attemptLogin(String UserName, String attemptPassword) {
+        if (userName.equals(UserName) && password == attemptPassword.hashCode()) 
+            return true;
+        else 
+            return false;
+    }
+}
+
+
