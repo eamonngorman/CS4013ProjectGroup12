46771ca89533752f705397a809d607cc59ba44b3
Liam Murray <LiamMurray3195@gmail.com>
2022-11-21 20:57:56 +0000

    test

--- src2/test.java
+++ src2/test.java
@@ -1,1 +1,10 @@
 
+public class Test {
+    public static void main(String[] args) {
+
+        long id = 900000000;
+        long access = id / 100000000;
+
+        System.out.println(access);
+    }
+}
