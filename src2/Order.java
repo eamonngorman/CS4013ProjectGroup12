3e4668a170f151de5132e41a14ff85d25b89d531
jamespb20 <skeletonporkchop20@gmail.com>
2022-11-22 13:42:42 +0000

    modified:   src2/Order.java

--- src2/Order.java
+++ src2/Order.java
@@ -5,11 +5,20 @@ public class Order {
     private int orderId;
     private ArrayList<MenuItem> itemsInOrder;
     private double totalCost;
+    private double tip;
+    private boolean isPaid;
 
     Order() {
         this.orderId = 123; //need way to create unique id
         this.itemsInOrder = new ArrayList<MenuItem>();
         this.totalCost = 0;
+    }
+
+    public void setTip(double tip){
+        this.tip = tip;
+    }
+    public void setPaid(boolean paid){
+        isPaid = paid;
     }
 
     public int getOrderId() {
@@ -32,5 +41,17 @@ public class Order {
     public void removeFromOrder(MenuItem item) {
         itemsInOrder.remove(item);
         totalCost -= item.getItemCost();
+    }
+
+    public void printBill (){  //method to print the bill. will only show tip after the bill has been paid
+        double beforeTip = totalCost;
+        totalCost += tip;
+        System.out.println("Yum Restaurant");
+        System.out.println(itemsInOrder);
+        System.out.println("Total: " + beforeTip);
+        if (isPaid == true){
+            System.out.println("Tip: " + tip);
+            System.out.println("Grand Total: " + totalCost);
+        }
     }
 }
