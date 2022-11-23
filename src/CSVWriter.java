dc5572d6fea9d353a9c0438e189a4ccb2c6c5c73
Eamonn <21204616@studentmail.ul.ie>
2022-11-23 15:55:21 +0000

    - Working on writeNewCustomerToCSV

--- src/CSVWriter.java
+++ src/CSVWriter.java
@@ -6,17 +6,19 @@ public class CSVWriter {
     public void writeNewCustomerToCSV(String[] details){
         PrintWriter printWriter;
         try {
-            File file = new File("customerDetails.csv");
+            File file = new File("personDetails.csv");
             printWriter = new PrintWriter(new FileWriter(file, true));
             Scanner input = new Scanner(file);
             StringBuffer csvData = new StringBuffer("");
 
             if (!input.hasNext()){
                 StringBuffer header = new StringBuffer("");
-                header.append("Name, Username, Password\n");
+                header.append("Name, Role, Username, Password\n");
                 printWriter.write(header.toString());
             }
             csvData.append(details[0]);
+            csvData.append(",");
+            csvData.append("Customer");
             csvData.append(",");
             csvData.append(details[1]);
             csvData.append(",");
