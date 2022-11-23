f06f92d196b6bc9e4e96f3f6086aaa862b05673a
Eamonn <21204616@studentmail.ul.ie>
2022-11-23 14:29:46 +0000

    - Added RestaurantChain constructor and getRestaurants()

--- src/RestaurantChain.java
+++ src/RestaurantChain.java
@@ -2,4 +2,13 @@ import java.util.ArrayList;
 
 public class RestaurantChain {
     ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+
+    public RestaurantChain(){
+        // ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+
+    }
+
+    public ArrayList<Restaurant> getRestaurants() {
+        return restaurants;
+    }
 }
